0f179fc231ba5fdf00880e0eade849d97c5b1675
Arpit Dubey <aarpitdubey@gmail.com>
2022-07-18 19:26:07 +0530

    CI CD Pipeline

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app=Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    return "Started Flask app"
+    return "CI CD pipeline has been established."
 
 if __name__=="__main__":
     app.run(debug=True)
