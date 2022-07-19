50854c93c9fa57931fd844a342c2b3a8aa4122bd
Arpit Dubey <aarpitdubey@gmail.com>
2022-07-19 18:56:07 +0530

    ML Project Folder&File Structuring

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from distutils.log import debug
 from flask import Flask
-
+from censusip.logger import logging
 app=Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
+    logging.info("We are testing logging module")
     return "CI CD pipeline has been established."
 
 if __name__=="__main__":
--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from xml.etree.ElementTree import VERSION
-from setuptools import setup
+from setuptools import setup, find_packages
 from typing import List
 
 # Declaring variables for setup functions
-PROJECT_NAME="adult-census-ip"
+PROJECT_NAME="censusip"
 VERSION="0.0.1"
 AUTHOR="Arpit Dubey"
 DESCRIPTION="This is internship Project for FSDS Nov batch Machine Learning Project."
 # PACKAGES=["censusip"]
+LICENCE = "Apache License"
+# PLATFORM = "Github, Docker, Heroku"
 REQUIREMENT_FILE_NAME="requirements.txt"
 
 def get_requirements_list()->List[str]:
@@ -19,13 +21,16 @@ def get_requirements_list()->List[str]:
     of libraries mentioned in requirements.txt
     """
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
-        return requirement_file.readlines()
+        return requirement_file.readlines().remove("-e .")
 
 setup(
     name=PROJECT_NAME,
     version=VERSION,
     author=AUTHOR,
     description=DESCRIPTION,
-    packages=PACKAGES,
+    # packages=PACKAGES,
+    packages=find_packages(),
+    licence = LICENCE,
+    # Platform=PLATFORM,
     install_requires=get_requirements_list()
 )
